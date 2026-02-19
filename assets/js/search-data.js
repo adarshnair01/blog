71e5234508205244ce984c94cd132ad866397e51
adarshnair01 <adarshnair01@gmail.com>
2026-02-19 10:54:35 +0000

    Deploying to gh-pages from @ adarshnair01/blog@b13c0ba6adca20e810dad98e4c32f06d7e4800ff 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -8136,21 +8136,28 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/blog/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/blog/assets/pdf/adarsh_nair_resume.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%64%61%72%73%68%6E%61%69%72%30%31@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/adarshnair01", "_blank");
+        },
+      },{
+        id: 'social-work',
+        title: 'Work',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://adarshnair.online", "_blank");
         },
       },{
         id: 'social-rss',
@@ -8165,13 +8172,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
